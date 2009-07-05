9e5dc8daaf6cf1400fb1c4955c4a6b0c63b7e5e3
JoonCheol Park <jooncheol@gmail.com>
2009-07-05 15:13:37 +0000

    git-svn-id: file:///googlecode-twobody/trunk@12 e994e674-9762-4e2d-9f2e-ced472811f85

--- twobody_ko.ts
+++ twobody_ko.ts
@@ -6,32 +6,32 @@
     <message>
         <location filename="mainwindow.cpp" line="39"/>
         <source>Ready</source>
-        <translation type="unfinished"></translation>
+        <translation>준비</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="51"/>
         <source>About Twobody</source>
-        <translation type="unfinished"></translation>
+        <translation>투바디에 관하여</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="52"/>
         <source>The &lt;b&gt;Twobody&lt;/b&gt; is time synchronize utility for between two DSLRs.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;투바디&lt;/b&gt;는 DSLR간의 시간 동기화를 위한 유틸리티 입니다.</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="79"/>
         <source>Add pictures</source>
-        <translation type="unfinished"></translation>
+        <translation>사진 더하기</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="199"/>
         <source>%1 files added</source>
-        <translation type="unfinished"></translation>
+        <translation>%1개의 파일이 추가 되었습니다</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="201"/>
         <source>%1 files added, %2 files skipped</source>
-        <translation type="unfinished"></translation>
+        <translation>%1개의 파일이 추가 되었고, %2개의 파일이 무시 되었습니다</translation>
     </message>
 </context>
 </TS>
